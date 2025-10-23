bdee116b21226e72325c5592a59b094a75e0537b
sskutushev <sskutushev@gmail.com>
2025-10-23 17:54:57 +0300

    Подготовка проекта для деплоя на Vercel, исправлены пути к ассетам, обновлена конфигурация TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  // Настройки для GitHub Pages
+  // Настройки для Vercel
   build: {
     rollupOptions: {
       output: {
